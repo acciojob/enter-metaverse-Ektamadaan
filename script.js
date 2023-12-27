50920b2941d64296eca36b6da390191c72dc6933
Ektamadaan <ektamadaan45@gmail.com>
2023-12-27 22:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your JS code here. If required.
+function getFormvalue() {
+    //Write your code here
+	let input1 = document.getElementsById("status");
+	
+	document.addEventListener("click" , function() {
+		input1.textContent("Entered Metaverse");
+		input1.tagName = "h1";
+	})
+}
 
