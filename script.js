a064e20ae962f1cacf0e1807da54b564d276546a
Ektamadaan <ektamadaan45@gmail.com>
2023-12-27 22:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getFormvalue() {
 	let input1 = document.getElementById("status");
 	let input2 = document.getElementById("enterBtn");
 
-	document.addEventListener("click" , function() {
+	input2.addEventListener("click" , function() {
 		input1.textContent = "Entered Metaverse";
 		var h1Element = document.createElement("h1");
 		h1Element.textContent = input1.textContent;
